df767637fed404f728ad67d3497b02b810fda2c4
Sascha Brüggenolte <sb@netinventors.de>
2016-10-27 11:01:03 +0200

    prevent installation, if vendor is missing

--- NetiFacebookSdk.php
+++ NetiFacebookSdk.php
@@ -8,8 +8,22 @@
 namespace NetiFacebookSdk;
 
 use Shopware\Components\Plugin;
+use Shopware\Components\Plugin\Context\InstallContext;
 
 class NetiFacebookSdk extends Plugin
 {
     const SDK_VERSION = '5.3.1';
+
+    /**
+     * @param InstallContext $context
+     * @throws \Exception
+     */
+    public function install(InstallContext $context)
+    {
+        if (! is_readable(__DIR__ . DIRECTORY_SEPARATOR . 'vendor')) {
+            throw new \Exception('Please run "composer install" before you install the plugin!');
+        }
+
+        parent::install($context);
+    }
 }
